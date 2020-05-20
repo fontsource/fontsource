7e24754ac4ab73bc4a0d96d96ddfeb24a5071a79
Lotus <DecliningLotus@users.noreply.github.com>
2020-05-20 11:51:21 +0300

    docs: update readme and template with badges

--- scripts/google/templates.js
+++ scripts/google/templates.js
@@ -39,6 +39,7 @@ exports.fontFace = _.template(
 
 exports.readme = _.template(
   `# Fontsource <%= fontName %>
+[![npm version](https://badge.fury.io/js/fontsource-<%= fontId %>.svg)](https://github.com/DecliningLotus/fontsource) [![Generic badge](https://img.shields.io/badge/fontsource-passing-brightgreen)](https://github.com/DecliningLotus/fontsource) [![GitHub stars](https://img.shields.io/github/stars/DecliningLotus/fontsource.svg?style=social&label=Star&maxAge=2592000)](https://GitHub.com/DecliningLotus/fontsource/stargazers/)
 
 The CSS and web font files to easily self-host the “<%= fontName %>” font. Please visit the main [Fontsource monorepo](https://github.com/DecliningLotus/fontsource) to view more details on this package.
 
