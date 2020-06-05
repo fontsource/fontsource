4f76ecab3e8b30b08786963ff6fdaa1b633e4821
Lotus <DecliningLotus@users.noreply.github.com>
2020-06-05 17:14:55 +0300

    fix: mentioned version in config comments

--- scripts/generic/config.js
+++ scripts/generic/config.js
@@ -1,5 +1,6 @@
 // defaultSubset is what is used to generate index.css
 // Keep unicoderange "null" if empty. Optional input.
+// version should match Github release version.
 // type helps identify the category of font, such as Google or not. Keep it "other" unless directed to do so.
 
 const config = {
