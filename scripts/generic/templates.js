f27ec772563cb01a8951cdbc1e4cad05ff2fe04d
Lotus <DecliningLotus@users.noreply.github.com>
2020-08-02 23:02:45 +0300

    feat: add generic font rebuilder

--- scripts/generic/templates.js
+++ scripts/generic/templates.js
@@ -3,7 +3,7 @@ const _ = require(`lodash`)
 exports.packageJson = _.template(
   `{
   "name": "fontsource-<%= fontId %>",
-  "version": "2.2.0",
+  "version": "3.0.0",
   "description": "<%= fontName %> font in NPM glory.",
   "main": "index.css",
   "keywords": [
@@ -18,8 +18,40 @@ exports.packageJson = _.template(
   ],
   "author": "Lotus <declininglotus@gmail.com>",
   "license": "MIT",
-  "repository": "https://github.com/fontsource/fontsource/tree/master/packages/<%= fontId %>",
-  "homepage": "https://github.com/fontsource/fontsource"
+  "homepage": "https://github.com/fontsource/fontsource/tree/master/packages/<%= fontId %>#readme",
+  "repository": {
+    "type" : "git",
+    "url" : "https://github.com/fontsource/fontsource.git",
+    "directory": "packages/<%= fontId %>"
+  }
+}
+`
+)
+
+exports.packageJsonRebuild = _.template(
+  `{
+  "name": "<%= name %>",
+  "version": "<%= version %>",
+  "description": "<%= fontName %> font in NPM glory.",
+  "main": "index.css",
+  "keywords": [
+    "fontsource",
+    "font",
+    "font family",
+    "<%= fontName %>",
+    "<%= fontId %>",
+    "css",
+    "front-end",
+    "web"
+  ],
+  "author": "Lotus <declininglotus@gmail.com>",
+  "license": "MIT",
+  "homepage": "https://github.com/fontsource/fontsource/tree/master/packages/<%= fontId %>#readme",
+  "repository": {
+    "type" : "git",
+    "url" : "https://github.com/fontsource/fontsource.git",
+    "directory": "packages/<%= fontId %>"
+  }
 }
 `
 )
@@ -34,16 +66,13 @@ exports.fontFace = _.template(
   src:
     url('<%= woff2Path %>') format('woff2'), /* Chrome 26+, Opera 23+, Firefox 39+ */
     url('<%= woffPath %>') format('woff'); /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
-  <% if (unicoderange !== "null") { %>
-  unicode-range: <%= unicoderange %>;
-<% } %>
 }
 `
 )
 
 exports.readme = _.template(
   `# Fontsource <%= fontName %>
-[![npm version](https://badge.fury.io/js/fontsource-<%= fontId %>.svg)](https://www.npmjs.com/package/fontsource-<%= fontId %>) [![Generic badge](https://img.shields.io/badge/fontsource-passing-brightgreen)](https://github.com/fontsource/fontsource) [![Monthly downloads](https://badgen.net/npm/dm/fontsource-<%= fontId %>)](https://github.com/fontsource/fontsource) [![Total downloads](https://badgen.net/npm/dt/fontsource-<%= fontId %>)](https://github.com/fontsource/fontsource) [![GitHub stars](https://img.shields.io/github/stars/DecliningLotus/fontsource.svg?style=social&label=Star)](https://github.com/fontsource/fontsource/stargazers)
+[![npm version](https://badge.fury.io/js/fontsource-<%= fontId %>.svg)](https://www.npmjs.com/package/fontsource-<%= fontId %>) [![Generic badge](https://img.shields.io/badge/fontsource-passing-brightgreen)](https://github.com/fontsource/fontsource) [![Monthly downloads](https://badgen.net/npm/dm/fontsource-<%= fontId %>)](https://github.com/fontsource/fontsource) [![Total downloads](https://badgen.net/npm/dt/fontsource-<%= fontId %>)](https://github.com/fontsource/fontsource) [![GitHub stars](https://img.shields.io/github/stars/fontsource/fontsource.svg?style=social&label=Star)](https://github.com/fontsource/fontsource/stargazers)
 
 The CSS and web font files to easily self-host the “<%= fontName %>” font. Please visit the main [Fontsource monorepo](https://github.com/fontsource/fontsource) to view more details on this package.
 
@@ -58,7 +87,7 @@ yarn add fontsource-<%= fontId %> // npm install fontsource-<%= fontId %>
 Then within your app entry file or site component, import it in. For example in Gatsby, you could choose to import it into a layout template (\`layout.js\`), page component, or \`gatsby-browser.js\`.
 
 \`\`\`javascript
-import "fontsource-<%= fontId %>" // require("fontsource-<%= fontId %>")
+import "fontsource-<%= fontId %>" // Defaults to weight 400 with all styles included.
 \`\`\`
 
 Fontsource allows you to select font subsets, weights and even individual styles, allowing you to cut down on payload sizes to the last byte! The default selection above, however, sticks to the Latin subset including all weights and styles.
