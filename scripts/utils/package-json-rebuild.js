42e10486138201b4e640415a2bc26cd42f7e3a61
Lotus <DecliningLotus@users.noreply.github.com>
2020-08-02 23:40:26 +0300

    feat: add packagejson rebuilder

---
+++ scripts/utils/package-json-rebuild.js
@@ -0,0 +1,33 @@
+const fs = require(`fs-extra`)
+const jsonfile = require(`jsonfile`)
+
+// Find names of all packages.
+const getDirectories = () =>
+  fs
+    .readdirSync("./packages", { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+const directories = getDirectories()
+
+directories.forEach(directory => {
+  const fontDir = `./packages/${directory}`
+  const metadata = jsonfile.readFileSync(`${fontDir}/metadata.json`)
+  const packageJSON = jsonfile.readFileSync(`${fontDir}/package.json`)
+  fs.removeSync(`${fontDir}/package.json`)
+  jsonfile.writeFileSync(`${fontDir}/package.json`, {
+    name: packageJSON.name,
+    version: packageJSON.version,
+    description: packageJSON.description,
+    main: "index.css",
+    keywords: packageJSON.keywords,
+    author: "Lotus <declininglotus@gmail.com>",
+    license: "MIT",
+    homepage: `https://github.com/fontsource/fontsource/tree/master/packages/${metadata.fontId}#readme`,
+    repository: {
+      type: "git",
+      url: "https://github.com/fontsource/fontsource.git",
+      directory: `packages/${metadata.fontId}`,
+    },
+  })
+})
