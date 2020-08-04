fdd6acab8f520d8b0e540c232fd1e1d3d12eeeaa
Lotus <DecliningLotus@users.noreply.github.com>
2020-08-04 19:49:50 +0300

    feat: add FiraGO font

--- scripts/generic/generic-font-packager.js
+++ scripts/generic/generic-font-packager.js
@@ -31,7 +31,7 @@ glob(fontFileDir + "/**/*.woff2", {}, (err, files) => {
 
   files.forEach(file => {
     // Remove file path and extension. 34 characters to account for scripts/generic/... filepath
-    name = file.slice(34 + fontId.length, -6).split("-")
+    name = file.slice(23 + fontId.length, -6).split("-")
     styles.push(name.slice(-1)[0])
     name.pop()
     weights.push(name.slice(-1)[0])
@@ -69,7 +69,7 @@ glob(fontFileDir + "/**/*.woff2", {}, (err, files) => {
       fs.writeFileSync(cssWeightPath, fileContentWeight)
 
       // index.css
-      if (subset === defSubset && weight == "400") {
+      if (subset === defSubset && (weight == "400" || weights.length == 1)) {
         fs.writeFileSync(`${fontDir}/index.css`, fileContentWeight)
       }
     })
