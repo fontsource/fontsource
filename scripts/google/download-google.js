d923357bc0d3f6af524473f7379f44af3950a6df
Lotus <declininglotus@gmail.com>
2020-08-18 19:25:17 +0300

    refactor: cleanup of code to become more modular (#53)
    * refactor: cleanup of code to become more modular
    
    * fix: comment out testing code
    
    Co-authored-by: Lotus <DecliningLotus@users.noreply.github.com>

--- scripts/google/download-google.js
+++ scripts/google/download-google.js
@@ -3,21 +3,18 @@ const async = require(`async`)
 const fs = require(`fs-extra`)
 const shell = require(`shelljs`)
 
-const fontsv2 = require(`google-font-metadata`)
-
+const fonts = require(`google-font-metadata`)
 const force = process.argv[2]
 
 fs.ensureDirSync(`packages`)
+fs.ensureDirSync(`scripts/temp_packages`)
 
 // Create an async queue object
 const processQueue = (fontid, cb) => {
   console.log(`Downloading ${fontid}`)
-  shell.exec(
-    `node ./scripts/google/google-font-packager-v2.js ${fontid} ${force}`,
-    () => {
-      cb()
-    }
-  )
+  shell.exec(`node ./scripts/google/run.js ${fontid} ${force}`, () => {
+    cb()
+  })
 }
 
 // EventEmitter listener is usually set at a default limit of 10, below chosen 12 concurrent workers
@@ -26,7 +23,7 @@ const queue = async.queue(processQueue, 12)
 
 queue.drain(() => {
   console.log(
-    `All ${Object.keys(fontsv2).length} Google Fonts V2 have been processed.`
+    `All ${Object.keys(fonts).length} Google Fonts have been processed.`
   )
 })
 
@@ -36,21 +33,13 @@ queue.error((err, fontid) => {
 
 // Testing
 /* const test = () => {
-  queue.push(`roboto`) // 7 subsets, 12 styles
-  queue.push(`orienta`) // 1 subset, 2 styles
-  queue.push(`arbutus`) // 2 subsets, 1 style
-  queue.push(`mate-sc`) // 1 subset, 1 styles
-  queue.push(`open-sans`)
-  queue.push(`lato`)
-  queue.push(`montserrat`)
-  queue.push(`noticia-text`)
   queue.push(`noto-sans-jp`)
 }
 test() */
 
 // Production
 const production = () => {
-  _.forOwn(fontsv2, font => {
+  _.forOwn(fonts, font => {
     queue.push(`${font.id}`)
   })
 }
