4f4fb57790aaa25976e8b22aa9408064f1dc44f8
Lotus <declininglotus@gmail.com>
2021-02-06 16:13:12 +0300

    feat: add variable scss mixins (#147)
    * feat: add variable scss mixins
    
    * docs: add relevant readme adjustments for feature
    
    Co-authored-by: Lotus <DecliningLotus@users.noreply.github.com>

--- scripts/templates/scss.js
+++ scripts/templates/scss.js
@@ -24,18 +24,16 @@ $unicodeMap: (<%= unicodeMap %>) !default;
       font-style: $style;
       font-display: $display;
       font-weight: $weight;
-      src: url("#{$fontDir}/#{$fontId}-#{$subset}-#{$weight}-#{$style}.woff2")
-          format("woff2"),
-        url("#{$fontDir}/#{$fontId}-all-#{$weight}-#{$style}.woff")
-          format("woff");
+      src: url("#{$fontDir}/#{$fontId}-#{$subset}-#{$weight}-#{$style}.woff2") format("woff2"),
+        url("#{$fontDir}/#{$fontId}-all-#{$weight}-#{$style}.woff") format("woff");
       unicode-range: $unicodeRangeValues;
     }
   }
 }
 
-$fontDir: "~@fontsource/#{$fontId}/files" !default;
-$woff2Path: "#{$fontDir}/#{$fontId}-<%= defSubset %>-#{$weight}-#{$style}.woff2" !default;
-$woffPath: "#{$fontDir}/#{$fontId}-<%= defSubset %>-#{$weight}-#{$style}.woff" !default;
+$defSubset: "<%= defSubset %>" !default;
+$woff2Path: "#{$fontDir}/#{$fontId}-#{$defSubset}-#{$weight}-#{$style}.woff2" !default;
+$woffPath: "#{$fontDir}/#{$fontId}-#{$defSubset}-#{$weight}-#{$style}.woff" !default;
 $unicodeRange: false !default;
 $unicodeRangeValues: (<%= defUnicode %>) !default;
 
@@ -61,6 +59,64 @@ $unicodeRangeValues: (<%= defUnicode %>) !default;
     }
   }
 }
+
+<% if (variableFlag) { %>$fontName: "<%= fontName %>Variable" !default;
+$weight: <%= variableWeight %> !default;
+$type: "wghtOnly" !default;
+$stretch: <%= variableWdth %> !default;
+  
+@mixin fontFaceVariable(
+  $fontName: $fontName,
+  $fontId: $fontId,
+  $style: $style,
+  $display: $display,
+  $weight: $weight,
+  $fontDir: $fontDir,
+  $type: $type,
+  $stretch: $stretch,
+  $unicodeMap: $unicodeMap
+) {
+  @each $subset, $unicodeRangeValues in $unicodeMap {
+    @font-face {
+      font-family: "#{$fontName}";
+      font-style: $style;
+      font-display: $display;
+      font-weight: $weight;
+      @if $type == "full" {
+        font-stretch: $stretch;
+      }
+      src: url("#{$fontDir}/#{$fontId}-#{$subset}-variable-#{$type}-#{$style}.woff2") format("woff2");
+      unicode-range: $unicodeRangeValues;
+    }
+  }
+}
+
+$woff2Path: "#{$fontDir}/#{$fontId}-#{$defSubset}-variable-#{$type}-#{$style}.woff2" !default;
+
+@mixin fontFaceVariableCustom(
+  $fontName: $fontName,
+  $fontId: $fontId,
+  $style: $style,
+  $display: $display,
+  $weight: $weight,
+  $woff2Path: $woff2Path,
+  $unicodeRange: $unicodeRange,
+  $unicodeRangeValues: $unicodeRangeValues
+) {
+  @font-face {
+    font-family: "#{$fontName}";
+    font-style: $style;
+    font-display: $display;
+    font-weight: $weight;
+    @if $type == "full" {
+      font-stretch: $stretch;
+    }
+    src: url("#{$woff2Path}") format("woff2");
+    @if $unicodeRange {
+      unicode-range: $unicodeRangeValues;
+    }
+  }
+}<% } %>
 `
 )
 
