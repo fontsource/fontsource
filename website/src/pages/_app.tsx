fd068ec7e14e2821fdecab705011387565df04b4
Lotus <DecliningLotus@users.noreply.github.com>
2021-05-05 12:39:19 +0300

    fix: resetting page state on dynamic route change

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -4,16 +4,20 @@ import "@fontsource/raleway/variable.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Next.js bug where page state is not reset - https://github.com/vercel/next.js/issues/9992
+  // Refer to components/FontPreview.tsx where the defPreviewText state isn't reset on route change
+  const { asPath } = useRouter();
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} key={asPath} />
     </ChakraProvider>
   );
 }
