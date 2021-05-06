64ca3fdb25faaaea5bc055bcfd7de154dc2ca01f
Lotus <DecliningLotus@users.noreply.github.com>
2021-05-06 13:54:45 +0300

    feat: add vercel branding

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -1,26 +1,55 @@
-import { Button, Flex, FlexProps, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  FlexProps,
+  Link,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Image from "next/image";
 import { AiFillGithub } from "react-icons/ai";
 
-export const Footer = (props: FlexProps) => (
-  <Flex
-    as="footer"
-    mt="auto"
-    px={8}
-    py={4}
-    width="100%"
-    justify="space-between"
-    maxWidth="72rem"
-    {...props}
-  >
-    <Link href="https://github.com/DecliningLotus" isExternal>
-      <Button variant="ghost" size="sm">
-        Developed by Lotus
-      </Button>
-    </Link>
-    <Link href="https://github.com/fontsource/fontsource" isExternal>
-      <Button variant="ghost" size="sm" rightIcon={<AiFillGithub />}>
-        GitHub
-      </Button>
-    </Link>
-  </Flex>
-);
+export const Footer = (props: FlexProps) => {
+  const logoURL = useColorModeValue(
+    "/vercel-logotype-dark.svg",
+    "/vercel-logotype-light.svg"
+  );
+  return (
+    <Flex
+      as="footer"
+      mt="auto"
+      px={8}
+      py={4}
+      width="100%"
+      align={{ base: "center", sm: "inherit" }}
+      justify={{ sm: "space-between" }}
+      maxWidth="72rem"
+      direction={{ base: "column", sm: "row" }}
+      {...props}
+    >
+      <Link href="https://github.com/DecliningLotus" isExternal>
+        <Button
+          variant="ghost"
+          size="sm"
+          rightIcon={
+            <Box ml={-2}>
+              <Image
+                src={logoURL}
+                height="18px"
+                width="100px"
+                priority={true}
+              />
+            </Box>
+          }
+        >
+          Powered by
+        </Button>
+      </Link>
+      <Link href="https://github.com/fontsource/fontsource" isExternal>
+        <Button variant="ghost" size="sm" rightIcon={<AiFillGithub />}>
+          GitHub
+        </Button>
+      </Link>
+    </Flex>
+  );
+};
