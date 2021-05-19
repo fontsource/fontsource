e513cdfd18392209282f7e873f42027670a66a98
Lotus <DecliningLotus@users.noreply.github.com>
2021-05-19 16:54:19 +0300

    feat: test utils

---
+++ tests/utils/utils.test.js
@@ -0,0 +1,39 @@
+const {
+  makeFontDownloadPath,
+  makeFontFilePath,
+  findClosest,
+} = require("../../scripts/utils/utils")
+
+const fontDir = "packages/noto-sans-jp"
+const fontId = "noto-sans-jp"
+const subset = "latin"
+const weight = "400"
+const style = "normal"
+const extension = "woff2"
+
+describe("Font paths", () => {
+  test("Generate download path", () => {
+    expect(
+      makeFontDownloadPath(fontDir, fontId, subset, weight, style, extension)
+    ).toBe("./packages/noto-sans-jp/files/noto-sans-jp-latin-400-normal.woff2")
+  })
+
+  test("Generate CSS path", () => {
+    expect(makeFontFilePath(fontId, subset, weight, style, extension)).toBe(
+      "./files/noto-sans-jp-latin-400-normal.woff2"
+    )
+  })
+})
+
+describe("Find closest available weights", () => {
+  test("400 is available", () => {
+    expect(findClosest(["300", "400", "500"], "400")).toBe("400")
+  })
+
+  test("400 is not available", () => {
+    // Rounds down by default
+    expect(findClosest(["200", "300", "500", "600"], "400")).toBe("300")
+    expect(findClosest(["200"], "400")).toBe("200")
+    expect(findClosest(["200", "500"], "400")).toBe("500")
+  })
+})
