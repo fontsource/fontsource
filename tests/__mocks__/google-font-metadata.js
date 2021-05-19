b248886347d21a02b4152b1775b2d9bcc03405ba
Lotus <DecliningLotus@users.noreply.github.com>
2021-05-19 16:48:15 +0300

    feat: add mocks and test result data

---
+++ tests/__mocks__/google-font-metadata.js
@@ -0,0 +1,5 @@
+const APIv1 = require("./google-font-metadata/google-fonts-v1.json")
+const APIv2 = require("./google-font-metadata/google-fonts-v2.json")
+const APIVariable = require("./google-font-metadata/variable.json")
+
+module.exports = { APIv1, APIv2, APIVariable }
