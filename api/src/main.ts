3981a3ff5eb6c7f30a7acff3b9bf45d242a048b2
Lotus <DecliningLotus@users.noreply.github.com>
2021-06-05 16:49:45 +0300

    feat: initial nestjs setup

---
+++ api/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(3000);
+}
+
+bootstrap();
