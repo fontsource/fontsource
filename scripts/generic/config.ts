a33368e5cd87523cd134125f98d244f13f013e14
DecliningLotus <declininglotus@gmail.com>
2021-09-15 18:02:23 +0000

    feat: add hauora sans

--- scripts/generic/config.ts
+++ scripts/generic/config.ts
@@ -3,14 +3,14 @@
 // type helps identify the category of font, such as Google or not. Keep it "other" unless directed to do so.
 
 const config = {
-  fontId: "hauora",
-  fontName: "Hauora",
+  fontId: "hauora-sans",
+  fontName: "Hauora Sans",
   defSubset: "latin",
   unicodeRange: {},
   version: "v1.001",
   category: "sans-serif",
-  sourcelink: "",
-  licenselink: "",
+  sourcelink: "https://github.com/WCYS-Co/Hauora-Sans",
+  licenselink: "https://github.com/WCYS-Co/Hauora-Sans/blob/master/OFL.txt",
   type: "other",
 };
 
