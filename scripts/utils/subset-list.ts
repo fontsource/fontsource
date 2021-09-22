c5de95f2e7fc9c798612e545b45be3862e05cfd9
DecliningLotus <declininglotus@gmail.com>
2021-09-22 16:37:26 +0100

    feat: add more language preview texts

---
+++ scripts/utils/subset-list.ts
@@ -0,0 +1,15 @@
+import jsonfile from "jsonfile";
+import path from "path";
+
+import { directories } from "./utils";
+
+const subsets: string[] = [];
+directories.forEach(directory => {
+  const metadataPath = path.join("./packages", directory, "metadata.json");
+  const metadata = jsonfile.readFileSync(metadataPath);
+  subsets.push(...metadata.subsets);
+});
+
+const noDuplicateSubsets = new Set(subsets);
+
+console.log(noDuplicateSubsets);
