2e3ace149cb424ceecd1ed6280755999c94027c7
Lotus <declininglotus@gmail.com>
2021-11-18 02:04:49 +0000

    docs(generic): update documentation for generic packager

--- scripts/generic/config.ts
+++ scripts/generic/config.ts
@@ -1,6 +1,12 @@
-// defaultSubset is what is used to generate index.css
-// version should match Github release version.
-// type helps identify the category of font, such as Google or not. Keep it "other" unless directed to do so.
+// fontId must be lowercase and uses dashes instead of spaces. It will also be used as the package name e.g. open-sans
+// fontName is what will be displayed publicly on the README and website e.g. Open Sans
+// defSubset is an abbreviation for the default subset. e.g. latin
+// unicodeRange is WIP and should be left as is.
+// version is the font source's version that will be shown on the README. e.g. v14
+// category is the type of font it is. e.g. sans-serif, serif, display, monospace, handwriting, other
+// sourcelink is the link to the source of the font. Typically a GitHub repo.
+// licenselink is the direct link that shows the license of the font, usually in the GitHub repo of the source.
+// type is fontsource-specific e.g. google, league, other. It should be left as "other" unless otherwise specified.
 
 const config = {
   fontId: "",
