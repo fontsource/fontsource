a6b81b21839cbd30256b208e54e3782e1ef4724c
Lotus <declininglotus@gmail.com>
2022-03-11 12:53:19 +0000

    fix: gracefully handle any read errors to fix builds

--- scripts/generic/rebuilder.ts
+++ scripts/generic/rebuilder.ts
@@ -26,47 +26,53 @@ interface UnicodeRange {
 
 directories.forEach(directory => {
   const fontDir = `./packages/${directory}`;
-  const metadata: Metadata = jsonfile.readFileSync(`${fontDir}/metadata.json`);
+  try {
+    const metadata: Metadata = jsonfile.readFileSync(
+      `${fontDir}/metadata.json`
+    );
 
-  let unicodeRange: UnicodeRange = {};
-  if (fs.existsSync(`${fontDir}/unicode.json`)) {
-    unicodeRange = jsonfile.readFileSync(`${fontDir}/unicode.json`);
-  }
+    let unicodeRange: UnicodeRange = {};
+    if (fs.existsSync(`${fontDir}/unicode.json`)) {
+      unicodeRange = jsonfile.readFileSync(`${fontDir}/unicode.json`);
+    }
 
-  // Rebuild only non-Google fonts
-  if (metadata.type !== "google") {
-    const packageJSONData = jsonfile.readFileSync(`${fontDir}/package.json`);
+    // Rebuild only non-Google fonts
+    if (metadata.type !== "google") {
+      const packageJSONData = jsonfile.readFileSync(`${fontDir}/package.json`);
 
-    // Clear directory
-    fs.copySync(`${fontDir}/files`, `./scripts/temp_packages/${directory}`);
-    fs.emptyDirSync(fontDir);
-    fs.copySync(`./scripts/temp_packages/${directory}`, `./${fontDir}/files`);
-    fs.removeSync(`./scripts/temp_packages/${directory}`);
+      // Clear directory
+      fs.copySync(`${fontDir}/files`, `./scripts/temp_packages/${directory}`);
+      fs.emptyDirSync(fontDir);
+      fs.copySync(`./scripts/temp_packages/${directory}`, `./${fontDir}/files`);
+      fs.removeSync(`./scripts/temp_packages/${directory}`);
 
-    // Create object to store all necessary data to run package function
-    const fontObject = {
-      fontId: metadata.fontId,
-      fontName: metadata.fontName,
-      subsets: metadata.subsets,
-      weights: metadata.weights.map(weight => Number(weight)),
-      styles: metadata.styles,
-      defSubset: metadata.defSubset,
-      unicodeRange,
-      variable: false,
-      lastModified: metadata.lastModified,
-      version: metadata.version,
-      category: metadata.category,
-      source: metadata.source,
-      license: metadata.license,
-      type: metadata.type,
+      // Create object to store all necessary data to run package function
+      const fontObject = {
+        fontId: metadata.fontId,
+        fontName: metadata.fontName,
+        subsets: metadata.subsets,
+        weights: metadata.weights.map(weight => Number(weight)),
+        styles: metadata.styles,
+        defSubset: metadata.defSubset,
+        unicodeRange,
+        variable: false,
+        lastModified: metadata.lastModified,
+        version: metadata.version,
+        category: metadata.category,
+        source: metadata.source,
+        license: metadata.license,
+        type: metadata.type,
 
-      fontDir,
-      packageVersion: packageJSONData.version,
-    };
+        fontDir,
+        packageVersion: packageJSONData.version,
+      };
 
-    // Generate files (true for rebuildFlag)
-    packager(fontObject, true);
+      // Generate files (true for rebuildFlag)
+      packager(fontObject, true);
 
-    console.log(`Finished processing ${metadata.fontId}.`);
+      console.log(`Finished processing ${metadata.fontId}.`);
+    }
+  } catch (error) {
+    console.error(error);
   }
 });
