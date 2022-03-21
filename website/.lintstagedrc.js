95d8099f4a584b7fefdb8d8cb94f6e0a8149b9d2
DecliningLotus <declininglotus@gmail.com>
2022-03-21 15:29:58 +0000

    configs

--- website/.lintstagedrc.js
+++ website/.lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': [`eslint --fix`],
   '*.{js,jsx,ts,tsx,css,md,json}': [`prettier --write`],
-  '**/*.ts?(x)': () => 'tsc -p ./website/tsconfig.json --noEmit',
+  '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
 }
