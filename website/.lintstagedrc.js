3e70238383fe42570dc4248b9d7938e48f4154c1
DecliningLotus <declininglotus@gmail.com>
2022-03-21 14:58:55 +0000

    feat: add lint staged configs

--- website/.lintstagedrc.js
+++ website/.lintstagedrc.js
@@ -1,11 +1,5 @@
-const path = require('path')
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
-
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  '*.{js,jsx,ts,tsx,css,md,json}': [`yarn format`]
+  '*.{js,jsx,ts,tsx}': [`eslint --fix`],
+  '*.{js,jsx,ts,tsx,css,md,json}': [`prettier --write`],
+  '**/*.ts?(x)': () => 'tsc -p ./website/tsconfig.json --noEmit',
 }
