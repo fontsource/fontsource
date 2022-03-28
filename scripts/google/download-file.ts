2a83bb44883a8028bb02f475189041696888f6a8
Lotus <declininglotus@gmail.com>
2022-03-28 01:15:38 +0100

    fix(repo): revert replace got with axios (#473)
    This reverts commit 9496fdb22396fc65b6b121da169eaedad3a7ac6c.

--- scripts/google/download-file.ts
+++ scripts/google/download-file.ts
@@ -1,11 +1,10 @@
 import _ from "lodash";
 import async from "async";
-import axios from "axios";
-import axiosRetry from "axios-retry";
 import flatten from "flat";
 import fs from "fs-extra";
 import isAbsoluteUrl from "is-absolute-url";
 import { APIv1, APIv2, APIVariable } from "google-font-metadata";
+import got from "got";
 import { EventEmitter } from "events";
 
 import type { FontVariants, FontVariantsVariable } from "google-font-metadata";
@@ -17,9 +16,8 @@ import {
 
 const gotDownload = async (url: string, dest: fs.PathLike): Promise<void> => {
   try {
-    axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay });
-    const response = await axios.get(url, { responseType: "arraybuffer" });
-    fs.writeFileSync(dest, response.data);
+    const response = await got(url).buffer();
+    fs.writeFileSync(dest, response);
   } catch (error) {
     console.log(error);
   }
