34ad5dbf310147e234b46fa51241da1d584f7f26
Anurag <kr.anurag24@gmail.com>
2022-04-13 13:40:32 +0000

    feat: add ctrl/cmd + k combination to trigger search modal

--- website/src/components/Search/Modal.tsx
+++ website/src/components/Search/Modal.tsx
@@ -1,14 +1,17 @@
 import { SearchIcon } from "@chakra-ui/icons";
 import {
   Button,
+  Flex,
+  Kbd,
   Modal,
   ModalBody,
   ModalContent,
   ModalOverlay,
   useColorModeValue,
   useDisclosure,
+  useEventListener,
 } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { getUrlParams } from "../../utils/getUrlParams";
 import { FontSearch } from "./InstantSearch";
@@ -16,6 +19,20 @@ import { FontSearch } from "./InstantSearch";
 export const SearchModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const bgSearch = useColorModeValue("gray.50", "gray.900");
+
+  const [isOnMacEnv, setOnMacEnv] = useState<boolean>();
+
+  useEffect(() => {
+    setOnMacEnv(/(Mac|iPhone|iPad|iPod)/i.test(navigator.userAgent));
+  }, [isOnMacEnv]);
+
+  useEventListener("keydown", (e) => {
+    const hotkey = isOnMacEnv ? "metaKey" : "ctrlKey";
+    if (e.key.toLowerCase() === "k" && e[hotkey]) {
+      e.preventDefault();
+      isOpen ? onClose() : onOpen();
+    }
+  });
 
   // Open modal if search url param exists (used for OpenSearch)
   useEffect(() => {
@@ -28,9 +45,11 @@ export const SearchModal = () => {
   return (
     <>
       <Button
-        leftIcon={<SearchIcon ml="-5px" mr="10px" />}
+        display="flex"
+        flexDir="row"
+        alignItems="center"
         onClick={onOpen}
-        justifyContent="left"
+        justifyContent={{ base: "left", lg: "space-between" }}
         width="100%"
         size="lg"
         bg={bgSearch}
@@ -40,7 +59,15 @@ export const SearchModal = () => {
         aria-label="Search fonts"
         role="search"
       >
-        Search
+        <Flex alignItems="center" gap="3">
+          <SearchIcon />
+          Search
+        </Flex>
+
+        <Flex gap="1" display={{ base: "none", lg: "flex" }}>
+          {isOnMacEnv ? <Kbd>cmd</Kbd> : <Kbd>ctrl</Kbd>}
+          <Kbd>k</Kbd>
+        </Flex>
       </Button>
 
       <Modal isOpen={isOpen} onClose={onClose} size="xl">
