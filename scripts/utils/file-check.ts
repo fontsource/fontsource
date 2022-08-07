b287458313b20c287fa0305142dae52f88b9f015
Ayu <declininglotus@gmail.com>
2022-08-07 22:39:07 +0300

    fix: improve error messages for file check (#535)

--- scripts/utils/file-check.ts
+++ scripts/utils/file-check.ts
@@ -50,28 +50,28 @@ const downloadFileCheck = (
           console.log(message);
           fontIds.push(path.basename(changedPackage));
         }
-      }
+      } else {
+        // Read file that compares
+        const files: string[] = jsonfile.readFileSync(
+          path.join(changedPackage, "files", "file-list.json")
+        );
 
-      // Read file that compares
-      const files: string[] = jsonfile.readFileSync(
-        path.join(changedPackage, "files", "file-list.json")
-      );
-
-      // Check binary files
-      for (const file of files) {
-        if (!fs.existsSync(file)) {
-          const message = `${file} does not exist`;
-          if (throwError) {
-            errors.push(message);
-          } else {
-            console.log(`${file} does not exist`);
-            fontIds.push(path.basename(changedPackage));
+        // Check binary files
+        for (const file of files) {
+          if (!fs.existsSync(file)) {
+            const message = `${file} does not exist`;
+            if (throwError) {
+              errors.push(message);
+            } else {
+              console.log(message);
+              fontIds.push(path.basename(changedPackage));
+            }
           }
         }
       }
     }
   }
-  if (errors.length > 0) throw new Error(errors.join("\n"));
+  if (errors.length > 0) throw new Error(`Errors:\n${errors.join("\n")}`);
   // Remove duplicates
   return [...new Set(fontIds)];
 };
