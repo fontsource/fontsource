b67e7f7e0f4de65069a6fa0e84994b67d853f0c9
Ayuhito <hello@ayuhito.com>
2022-10-24 14:33:18 +0100

    test: fix coverage for license file

--- utils/cli/tests/google/license.test.ts
+++ utils/cli/tests/google/license.test.ts
@@ -8,7 +8,7 @@ vi.mock('fs-extra');
 vi.mock('got');
 
 describe('generate license', () => {
-	it('should generate license successfully', async () => {
+	it('should generate license successfully (apache)', async () => {
 		const buildOpts = {
 			dir: 'test/noto-sans-jp',
 			tmpDir: 'test',
@@ -24,6 +24,64 @@ describe('generate license', () => {
 			'noto-sans-jp',
 			'apache license, version 2.0',
 			buildOpts
+		);
+
+		expect(vi.mocked(got.get)).toHaveBeenCalledWith(
+			'https://cdn.jsdelivr.net/gh/google/fonts@main/apache/notosansjp/LICENSE.txt'
+		);
+		expect(vi.mocked(fs.writeFile)).toBeCalledWith(
+			'test/noto-sans-jp/LICENSE',
+			'test'
+		);
+	});
+
+	it('should generate license successfully (ofl)', async () => {
+		const buildOpts = {
+			dir: 'test/noto-sans-jp',
+			tmpDir: 'test',
+			force: false,
+			isVariable: false,
+		};
+
+		vi.spyOn(got, 'get').mockReturnValue({
+			body: 'test',
+		} as unknown as Request);
+
+		await generateLicense(
+			'noto-sans-jp',
+			'sil open font license, 1.1',
+			buildOpts
+		);
+
+		expect(vi.mocked(got.get)).toHaveBeenCalledWith(
+			'https://cdn.jsdelivr.net/gh/google/fonts@main/ofl/notosansjp/OFL.txt'
+		);
+		expect(vi.mocked(fs.writeFile)).toBeCalledWith(
+			'test/noto-sans-jp/LICENSE',
+			'test'
+		);
+	});
+
+	it('should generate license successfully (ufl)', async () => {
+		const buildOpts = {
+			dir: 'test/noto-sans-jp',
+			tmpDir: 'test',
+			force: false,
+			isVariable: false,
+		};
+
+		vi.spyOn(got, 'get').mockReturnValue({
+			body: 'test',
+		} as unknown as Request);
+
+		await generateLicense(
+			'noto-sans-jp',
+			'ubuntu font license, 1.0',
+			buildOpts
+		);
+
+		expect(vi.mocked(got.get)).toHaveBeenCalledWith(
+			'https://cdn.jsdelivr.net/gh/google/fonts@main/ufl/notosansjp/UFL.txt'
 		);
 		expect(vi.mocked(fs.writeFile)).toBeCalledWith(
 			'test/noto-sans-jp/LICENSE',
@@ -45,6 +103,21 @@ describe('generate license', () => {
 
 		await expect(
 			generateLicense('noto-sans-jp', 'unknown license', buildOpts)
+		).rejects.toThrow();
+	});
+
+	it('should throw error when got fails to fetch the license', async () => {
+		const buildOpts = {
+			dir: 'test/noto-sans-jp',
+			tmpDir: 'test',
+			force: false,
+			isVariable: false,
+		};
+
+		vi.spyOn(got, 'get').mockRejectedValue(new Error('test'));
+
+		await expect(
+			generateLicense('noto-sans-jp', 'sil open font license, 1.1', buildOpts)
 		).rejects.toThrow();
 	});
 });
