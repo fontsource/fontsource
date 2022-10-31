859d20aef85ba7d8300e785c5b3d6fcc38b6d6be
Lotus <declininglotus@gmail.com>
2022-10-31 21:05:59 +0000

    fix: replace naive implementation of css minify

--- website/app/routes/fonts/$id/fetch-css.ts
+++ website/app/routes/fonts/$id/fetch-css.ts
@@ -9,11 +9,22 @@ export const loader: LoaderFunction = async ({ params }) => {
   const rawCss = await fetch(cssUrl(params.id as string)).then(res =>
     res.text()
   );
-  const css = rawCss.replace(
+
+  const cssRewrite = rawCss.replace(
     /url\('\.\/(files\/.*?)'\)/g,
     // match "url('./files/${woffFileName}')", then replace with "url('${baseURL}/files/${woffFileName}')"
     `url('${baseUrl}/@fontsource/${params.id}/$1')`
-    // Replace all whitespace to minify css
-  ).replace(/\s/g, '');
-  return json({ css });
+  );
+
+  const minifiedCss = cssRewrite
+    // remove comments, newlines, and tabs
+    .replace(/\/\*[\s\S]*?\*\/|[\r\n\t]+/g, "")
+    // limit number of adjacent spaces to 1
+    .replace(/ {2,}/g, " ")
+    // remove spaces around the following: ,:;{}
+    .replace(/ ?([,:;{}]) ?/g, "$1")
+    // remove last semicolon in block
+    .replace(/;}/g, "}");
+
+  return json({ css: minifiedCss });
 };
