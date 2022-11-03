230439b46e37c0171303dfcaf5f370df3785f273
Ayu <hello@ayuhito.com>
2022-11-03 14:57:09 +0000

    fix: remove generate multi and clean up some interfaces (#595)
    * fix: remove generate multi and clean up some interfaces
    
    * test: rewrite tests

--- utils/generate/src/utils.ts
+++ utils/generate/src/utils.ts
@@ -1,2 +1,10 @@
+import { Axes } from './types';
+
 // Find the lowest and highest value in a weight array
-export const findBoundaries = (weights: number[]) => [Math.min(...weights), Math.max(...weights)];
+export const getVariableWght = (axes: Axes) => {
+	if (!axes) return '400';
+
+	if (axes.min === axes.max) return `${axes.min}`;
+
+	return `${axes.min} ${axes.max}`;
+};
