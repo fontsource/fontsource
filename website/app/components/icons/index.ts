b435564f655d038b0693cfdc5adf482310f88b52
DecliningLotus <declininglotus@gmail.com>
2022-12-30 16:11:07 +0700

    feat: add more icons

--- website/app/components/icons/index.ts
+++ website/app/components/icons/index.ts
@@ -1,9 +1,12 @@
-export { IconCaret } from './Caret'
+export { IconCaret } from './Caret';
 export { IconDiscord } from './Discord';
+export { IconEye } from './Eye';
 export { IconGithub } from './Github';
-export { IconGrid } from './Grid'
-export { IconList } from './List'
+export { IconGrid } from './Grid';
+export { IconHorizontal } from './Horizontal';
+export { IconList } from './List';
 export { IconMoon } from './Moon';
-export { IconSearch } from './Search'
+export { IconSearch } from './Search';
 export { IconSun } from './Sun';
-export { IconTrash } from './Trash'
+export { IconTrash } from './Trash';
+export { IconVertical } from './Vertical';
