d08a3bbe034d44c3adf562457c5059ea2e424e86
Johnwesley R <jwr12135@gmail.com>
2023-02-23 15:03:37 -0500

    Fix website deployments (#638)

--- website/next.config.js
+++ website/next.config.js
@@ -1,5 +1,9 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    largePageDataBytes: 8000000, // 8MB
+  },
   async redirects() {
     return [
       {
