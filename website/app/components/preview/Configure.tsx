4bef4519248e2732914b088f9d3e5818e4c5b400
Lotus <declininglotus@gmail.com>
2023-03-02 09:31:34 +0000

    feat: add size slider

--- website/app/components/preview/Configure.tsx
+++ website/app/components/preview/Configure.tsx
@@ -1,13 +1,41 @@
-import { Box, Checkbox, createStyles, Divider, Text } from '@mantine/core';
+import {
+	ActionIcon,
+	Box,
+	Checkbox,
+	createStyles,
+	Divider,
+	Text,
+} from '@mantine/core';
 import { atom, useAtom } from 'jotai';
 
 import type { Metadata, VariableData } from '@/utils/types';
 
+import { IconItalic } from '../icons/Italic';
 import { SizeSlider } from './SizeSlider';
 
 const useStyles = createStyles((theme) => ({
 	wrapper: {
+		display: 'flex',
+		flexDirection: 'column',
 		width: '332px',
+		height: '50vh',
+		padding: '24px',
+		border: `1px solid ${
+			theme.colorScheme === 'dark'
+				? theme.colors.border[1]
+				: theme.colors.border[0]
+		}`,
+		borderRadius: '4px',
+	},
+
+	title: {
+		fontSize: '15px',
+		fontWeight: 700,
+		color:
+			theme.colorScheme === 'dark'
+				? theme.colors.text[0]
+				: theme.colors.text[1],
+		lineHeight: '18px',
 	},
 }));
 
@@ -21,10 +49,10 @@ const Configure = ({ metadata, variable }: ConfigureProps) => {
 	return (
 		<Box className={classes.wrapper}>
 			<Box>
-				<Text>Settings</Text>
-				<Box>
+				<Text className={classes.title}>Settings</Text>
+				<Text>Language</Text>
 					<SizeSlider />
-				</Box>
+
 				<Box>
 					<Text>Line Height</Text>
 					<Text>Letter Spacing</Text>
@@ -36,7 +64,9 @@ const Configure = ({ metadata, variable }: ConfigureProps) => {
 				{metadata.variable && (
 					<>
 						<Divider />
-						<Text>Variable Axes {JSON.stringify(variable)}</Text>
+						<Text className={classes.title}>
+							Variable Axes {JSON.stringify(variable)}
+						</Text>
 						{Object.keys(variable).map((key) => (
 							<Box key={key}>{key}</Box>
 						))}
