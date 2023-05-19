5e9064018355062887a6e480d73b1a11bdeea154
Lotus <declininglotus@gmail.com>
2023-05-19 16:33:13 +0100

    feat: add favicon

--- website/app/root.tsx
+++ website/app/root.tsx
@@ -53,6 +53,27 @@ export const links: LinksFunction = () => [
 		rel: 'preconnect',
 		href: 'https://cdn.jsdelivr.net/',
 	},
+	{
+		rel: 'apple-touch-icon',
+		sizes: '180x180',
+		href: '/apple-touch-icon.png',
+	},
+	{
+		rel: 'icon',
+		type: 'image/png',
+		sizes: '32x32',
+		href: '/favicon-32x32.png',
+	},
+	{
+		rel: 'icon',
+		type: 'image/png',
+		sizes: '16x16',
+		href: '/favicon-16x16.png',
+	},
+	{
+		rel: 'manifest',
+		href: '/site.webmanifest',
+	},
 ];
 
 createEmotionCache({ key: 'mantine' });
