ed887ee9372b495801e8120421b93c05febb644a
Lotus <declininglotus@gmail.com>
2023-05-20 11:54:59 +0100

    perf: use queue and four hasher instances

--- packages/publish/src/changed.ts
+++ packages/publish/src/changed.ts
@@ -6,14 +6,35 @@ import colors from 'picocolors';
 import type { ChangedFlags, ChangedList, Context, PackageJson } from './types';
 import { getPackages, mergeFlags } from './utils';
 import { getHash, hasher } from './hash';
+import PQueue from 'p-queue';
 
 // Iterate through all packages in directory and return a list of changed packages
 const getChanged = async (ctx: Context) => {
 	const changedList: ChangedList = [];
-	const newHash = await hasher();
+	// Generate 4 hasher instances
+	const hashers = await Promise.all([hasher(), hasher(), hasher(), hasher()]);
+	const queue = new PQueue({ concurrency: 8 });
+
+	const handleHash = async (
+		packagePath: string,
+		packageJson: PackageJson,
+		hasher: number
+	) => {
+		// Get hash of current package and compare with old hash
+		const hash = await getHash(packagePath, hashers[hasher]);
+		if (packageJson.publishHash !== hash || ctx.forcePublish) {
+			changedList.push({
+				name: packageJson.name,
+				hash,
+				path: packagePath,
+				version: packageJson.version,
+			});
+		}
+	};
 
 	for (const packageDir of ctx.packages) {
 		const packages = await getPackages(packageDir);
+		let hasherIndex = 0;
 		for (const packageName of packages) {
 			const packagePath = path.join(packageDir, packageName);
 
@@ -22,19 +43,13 @@ const getChanged = async (ctx: Context) => {
 				path.join(process.cwd(), packagePath, 'package.json')
 			);
 
-			// Get hash of current package and compare with old hash
-			const hash = await getHash(packagePath, newHash);
-			if (packageJson.publishHash !== hash || ctx.forcePublish) {
-				changedList.push({
-					name: packageJson.name,
-					hash,
-					path: packagePath,
-					version: packageJson.version,
-				});
-			}
+			queue.add(() => handleHash(packagePath, packageJson, hasherIndex++));
+
+			if (hasherIndex === 4) hasherIndex = 0;
 		}
 	}
 
+	await queue.onIdle();
 	return changedList;
 };
 
