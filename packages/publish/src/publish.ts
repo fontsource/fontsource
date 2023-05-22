b058f9e74987f5991df9d95a3390cdcfd006ae72
Ayu <hello@ayuhito.com>
2023-05-22 19:30:56 +0100

    fix(publish): better handle errors from npm

--- packages/publish/src/publish.ts
+++ packages/publish/src/publish.ts
@@ -15,7 +15,7 @@ import {
 	gitPush,
 	gitRemoteAdd,
 } from './git';
-import type { BumpObject, PackageJson, PublishFlags } from './types';
+import type { BumpObject, Context, PackageJson, PublishFlags } from './types';
 import { mergeFlags } from './utils';
 import stringify from 'json-stringify-pretty-compact';
 import { confirm, isCancel } from '@clack/prompts';
@@ -54,7 +54,7 @@ interface PublishObject extends BumpObject {
 	error?: unknown;
 }
 
-const packPublish = async (pkg: BumpObject): Promise<void | PublishObject> => {
+const packPublish = async (pkg: BumpObject): Promise<PublishObject> => {
 	const npmVersion = `${pkg.name}@${pkg.bumpVersion}`;
 	const publishFlags = ['--access', 'public', '--tag', 'latest'];
 	try {
@@ -65,14 +65,51 @@ const packPublish = async (pkg: BumpObject): Promise<void | PublishObject> => {
 		await writeUpdate(pkg, { hash: true });
 	} catch (error) {
 		consola.error(`Failed to publish ${npmVersion}!`);
-		throw error;
+		// If we get rate limited, throw the error to stop the queue
+		// The retry delay should mean all other active publishes will also fail safely
+		// @ts-ignore - This is a custom error thrown by execa
+		if (error.message.contains('429 Too Many Requests')) {
+			throw error;
+		}
+
+		// Otherwise, just reject the promise and store the error message so we can print it later
+		return Promise.reject({
+			...pkg,
+			error,
+		});
 	}
 
 	consola.success(`Successfully published ${colors.green(npmVersion)}!`);
-	return undefined;
+	return Promise.resolve(pkg);
 };
 
-const queue = new PQueue({ concurrency: 8 });
+interface DoGitOptions {
+	name: string;
+	config: Context;
+	bumped: BumpObject[];
+	npmrc: string;
+}
+
+const doGitOperations = async ({
+	name,
+	config,
+	bumped,
+	npmrc,
+}: DoGitOptions) => {
+	// Cleanup .npmrc
+	await fs.remove(npmrc);
+
+	// Stage all files
+	await gitAdd();
+
+	// Commit changes
+	const commitMessage = getCommitMessage(config, bumped);
+	await gitCommit(commitMessage);
+
+	// Push changes
+	await gitRemoteAdd(name);
+	await gitPush();
+};
 
 export const publishPackages = async (
 	version: string,
@@ -83,6 +120,9 @@ export const publishPackages = async (
 			options.forcePublish ? colors.red(colors.bold('[FORCE]')) : ''
 		}`
 	);
+
+	const queue = new PQueue({ concurrency: 8 });
+
 	// Check for required environment variables
 	await checkEnv();
 	const { name } = await getGitConfig();
@@ -114,23 +154,39 @@ export const publishPackages = async (
 		}
 	}
 
-	await Promise.all(publishArr);
+	queue.on('error', async (error) => {
+		if (error.message.includes('429 Too Many Requests')) {
+			// We still want to commit the font changes and already published packages publish hashes
+			await doGitOperations({ name, config, bumped, npmrc });
+			throw error;
+		} else {
+			consola.error("This shouldn't happen.");
+			throw error;
+		}
+	});
 
-	// Cleanup .npmrc
-	await fs.remove(npmrc);
+	// We only want the build to crash on a 429 error
+	// Otherwise, we want to continue publishing, commit and print the errors at the end
+	const results = await Promise.allSettled(publishArr);
 
-	// Stage all files
-	await gitAdd();
+	// Print errors
+	const errors = results.filter((r) => r.status === 'rejected');
+	if (errors.length > 0) {
+		consola.error('Failed to publish the following packages:');
+		for (const error of errors) {
+			if (error.status !== 'rejected') continue; // This should never happen, but ts is complaining
+			const pkg = error.reason as PublishObject;
+			consola.error(
+				`${colors.bold(colors.red(pkg.name))} - ${colors.red(
+					(pkg.error as Error).message
+				)}`
+			);
+		}
+	}
 
-	// Commit changes
-	const commitMessage = getCommitMessage(config, bumped);
-	await gitCommit(commitMessage);
+	await doGitOperations({ name, config, bumped, npmrc });
 
-	// Push changes
-	await gitRemoteAdd(name);
-	await gitPush();
+	if (errors.length > 0) {
+		throw new Error('Failed to publish packages!');
+	}
 };
-
-queue.on('error', (error) => {
-	throw error;
-});
