1fb65b2fcab1523b972358fb53b77df351227e88
Ayu <hello@ayuhito.com>
2023-05-27 14:50:41 +0100

    chore: remove old dir

--- api/src/gridfs/gridfs.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { gridFSProviders } from './gridfs.providers';
-
-@Module({
-  providers: [...gridFSProviders],
-  exports: [...gridFSProviders],
-})
-export class GridFSModule {}
