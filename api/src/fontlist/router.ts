fadc036449dbfc4619d637e98c3a0fa82d6d5868
Ayu <hello@ayuhito.com>
2023-05-27 15:59:02 +0100

    feat: add fontlist

--- api/src/fontlist/router.ts
+++ api/src/fontlist/router.ts
@@ -1,7 +1,33 @@
 import { CFRouterContext } from '@/types';
-import { IRequest, Router } from 'itty-router';
+import { IRequestStrict, Router } from 'itty-router';
 
-const router = Router<IRequest, CFRouterContext>();
+const router = Router<IRequestStrict, CFRouterContext>();
+
+const listPlaceholder = {
+	abel: 'google',
+	'material-icons': 'icons',
+	yakuhanjp: 'other',
+};
+
+const listPlaceholderDate = {
+	abel: '2021-01-01',
+	'material-icons': '2021-01-01',
+	yakuhanjp: '2021-01-01',
+};
+
+router.get('/fontlist', async (request, env, ctx) => {
+	const url = new URL(request.url);
+
+	// If there is a query string ?date, return listPlaceholderDate else return listPlaceholder
+	const date = url.searchParams.get('date');
+	const list = date ? listPlaceholderDate : listPlaceholder;
+
+	return new Response(JSON.stringify(list), {
+		headers: {
+			'content-type': 'application/json;charset=UTF-8',
+		},
+	});
+});
 
 // 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }));
--- api/src/fonts/router.ts
+++ api/src/fonts/router.ts
@@ -1,7 +1,7 @@
-import { Router, IRequest } from 'itty-router';
+import { Router, IRequestStrict } from 'itty-router';
 import { CFRouterContext } from '@/types';
 
-const router = Router<IRequest, CFRouterContext>();
+const router = Router<IRequestStrict, CFRouterContext>();
 
 // 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }));
