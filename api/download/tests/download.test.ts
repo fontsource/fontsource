e3215f0ff609d0259d28204eefad9267c2a11ea8
Ayu <hello@ayuhito.com>
2023-05-28 15:38:12 +0100

    test: fix download tests

--- api/download/tests/download.test.ts
+++ api/download/tests/download.test.ts
@@ -6,10 +6,61 @@ describe('download worker', () => {
 	const env = getMiniflareBindings() as Env;
 	const ctx = new ExecutionContext();
 
+	beforeEach(async () => {
+		const fetchMock = getMiniflareFetchMock();
+		// Throw when no matching mocked request is found
+		fetchMock.disableNetConnect();
+
+		// Handlers
+		// We want this to be the default response for all requests
+		const origin = fetchMock.get('https://cdn.jsdelivr.net');
+		origin
+			.intercept({
+				method: 'GET',
+				path: '/npm/@fontsource/roboto@latest/files/roboto-latin-400-normal.woff2',
+			})
+			.reply(200, 'test');
+		origin
+			.intercept({
+				method: 'GET',
+				path: '/npm/@fontsource/roboto@latest/files/roboto-latin-400-normal.woff',
+			})
+			.reply(200, 'test');
+		origin
+			.intercept({
+				method: 'GET',
+				path: '/npm/@fontsource/roboto@latest/files/roboto-latin-400-normal.ttf',
+			})
+			.reply(200, 'test');
+	});
+
 	it('should return type as default request', async () => {
+		env.FONTS.put(
+			'roboto',
+			JSON.stringify({
+				id: 'roboto',
+				subsets: ['latin'],
+				weights: [400],
+				styles: ['normal'],
+			})
+		);
+
 		const request = new Request('http://localhost:8787/v1/download/roboto');
 		const response = await worker.fetch(request, env, ctx);
 
 		expect(response.status).toBe(200);
+		// Check uploaded files
+		expect((await env.BUCKET.list()).objects.map((item) => item.key)).toEqual([
+			'roboto@latest/download.zip',
+			'roboto@latest/latin-400-normal.woff',
+			'roboto@latest/latin-400-normal.woff2',
+		]);
+	});
+
+	it('should return 400 for invalid font id', async () => {
+		const request = new Request('http://localhost:8787/v1/download/invalid');
+		const response = await worker.fetch(request, env, ctx);
+
+		expect(response.status).toBe(404);
 	});
 });
