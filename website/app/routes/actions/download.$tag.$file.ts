5a6c01e6d27bbc8c753f15da5d29d4cc5e85ff04
DecliningLotus <declininglotus@gmail.com>
2023-09-16 23:37:13 +0300

    feat: add auth to website

--- website/app/routes/actions/download.$tag.$file.ts
+++ website/app/routes/actions/download.$tag.$file.ts
@@ -10,13 +10,28 @@ export const loader: LoaderFunction = async () => {
 };
 
 export const action: ActionFunction = async ({ request, params }) => {
-	const { tag, file } = params;
-	const header = request.headers.get('Authorization');
-	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-	if (!header || header !== `Bearer ${process.env.UPDATE_TOKEN!}`) {
-		throw new Response('Invalid update bearer token', { status: 401 });
+	const authHeader = request.headers.get('Authorization');
+	if (!authHeader) {
+		throw new Response('Unauthorized. Missing authorization header.', {
+			status: 401,
+		});
+	}
+	const [scheme, encoded] = authHeader.split(' ');
+
+	// The Authorization header must start with Bearer, followed by a space.
+	if (!encoded || scheme !== 'Bearer') {
+		throw new Response('Bad Request. Malformed authorization header.', {
+			status: 400,
+		});
 	}
 
+	if (encoded !== process.env.UPLOAD_KEY) {
+		throw new Response('Unauthorized. Invalid authorization token.', {
+			status: 401,
+		});
+	}
+
+	const { tag, file } = params;
 	if (!tag) {
 		throw new Response('Bad Request. Missing font tag in URL.', {
 			status: 400,
