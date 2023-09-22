bc30b4efa5268e8a21097a59ecfa3a944e21fe05
DecliningLotus <declininglotus@gmail.com>
2023-09-22 11:12:30 +0300

    feat: add a status page redirect

---
+++ website/app/routes/__boundary/status.tsx
@@ -0,0 +1,6 @@
+import type { LoaderFunction } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+
+export const loader: LoaderFunction = async () => {
+	return redirect('https://status.fontsource.org');
+};
