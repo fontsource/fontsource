0591a748f134470b3b49abc435df115fad4ba9dd
Ayu <hello@ayuhito.com>
2023-10-04 00:56:43 +0100

    perf(api): use jsdelivr cdn for font files in css (#867)
    * perf(api): use jsdelivr cdn for font files in css
    
    * chore: fix tests
    
    * Create slow-days-itch.md

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,4 +1,4 @@
 import { defineWorkspace } from 'vitest/config';
 
 // api is not included in workspace because Miniflare breaks
-export default defineWorkspace(['packages/*']);
+export default defineWorkspace(['packages/*', 'api/*']);
