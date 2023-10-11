deeb13b2685d4e977f07904395775eead38f4af1
Ayu <hello@ayuhito.com>
2023-10-11 16:02:26 +0100

    feat: add cdn page

---
+++ website/app/routes/fonts.$id.cdn.tsx
@@ -0,0 +1,57 @@
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import invariant from 'tiny-invariant';
+
+import { CDN } from '@/components/preview/CDN';
+import { TabsWrapper } from '@/components/preview/Tabs';
+import { ogMeta } from '@/utils/meta';
+import { getMetadata, getStats, getVariable } from '@/utils/metadata.server';
+import type { Metadata, VariableData } from '@/utils/types';
+
+interface FontMetadata {
+	metadata: Metadata;
+	variable?: VariableData;
+	hits: number;
+}
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+	const { id } = params;
+	invariant(id, 'Missing font ID!');
+
+	const metadata = await getMetadata(id);
+	const [variable, stats] = await Promise.all([
+		metadata.variable ? getVariable(id) : undefined,
+		getStats(id),
+	]);
+
+	const res: FontMetadata = {
+		metadata,
+		variable,
+		hits: stats.total.jsDelivrHitsTotal,
+	};
+
+	return json(res);
+};
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+	const title = data?.metadata.family
+		? `${data.metadata.family} | Fontsource`
+		: 'Fontsource';
+
+	const description = data?.metadata.family
+		? `Self-host ${data.metadata.family} in a neatly bundled package.`
+		: 'Self-host Open Source fonts in neatly bundled packages.';
+	return ogMeta({ title, description });
+};
+
+export default function CDNPage() {
+	const data = useLoaderData<FontMetadata>();
+	const { metadata, variable, hits } = data;
+
+	return (
+		<TabsWrapper metadata={metadata} tabsValue="cdn">
+			<CDN metadata={metadata} variable={variable} hits={hits} />
+		</TabsWrapper>
+	);
+}
