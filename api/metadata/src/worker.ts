91d0712feecdd90e46e55b4e69aa805933ccb8cd
Ayu <hello@ayuhito.com>
2024-02-09 02:10:26 +0000

    perf: add cron trigger to update metadata

--- api/metadata/src/worker.ts
+++ api/metadata/src/worker.ts
@@ -1,6 +1,11 @@
 import { error } from 'itty-router';
 
+import { fontlistQueries } from './fontlist/types';
+import { updateList, updateMetadata } from './fontlist/update';
+import { updateArrayMetadata } from './fonts/update';
 import { corsify, router } from './router';
+import { updatePackageStatAll } from './stats/update';
+import { updateAxisRegistry, updateVariableList } from './variable/update';
 
 export default {
 	async fetch(
@@ -15,5 +20,30 @@ export default {
 				return error(error_);
 			})
 			.then(corsify);
+	},
+
+	// This is a daily scheduled event to update the metadata.
+	async scheduled(
+		_event: ScheduledEvent,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<void> {
+		// Update fontlist and aggregate metadata object.
+		await updateMetadata(env, ctx);
+		for (const type of fontlistQueries) {
+			await updateList(type, env, ctx);
+		}
+
+		// Update the metadata arr. We'll let the rest of the individual font ids expire
+		// and update on their own.
+		await updateArrayMetadata(env, ctx);
+
+		// Update variable lists.
+		await updateAxisRegistry(env, ctx);
+		await updateVariableList(env, ctx);
+
+		// Update stats.
+		await updatePackageStatAll(env, ctx);
+		// TODO: Versions
 	},
 };
