501ad3be729e08bc2f94b0b78f796f71f3d0161c
Ayu <hello@ayuhito.com>
2024-04-14 03:34:47 +0100

    fix: add top level await plugin

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import { cjsInterop } from 'vite-plugin-cjs-interop';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 installGlobals();
@@ -25,6 +26,7 @@ export default defineConfig({
 				'react-wrap-balancer',
 			],
 		}),
+		topLevelAwait(),
 		tsconfigPaths(),
 	],
 });
