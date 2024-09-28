502a123383a7a4fdd402224694aa3c979e927c9d
ayuhito <hello@ayuhito.com>
2024-09-28 15:51:28 +0900

    feat: initial migration to cf workers backend

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -1,10 +1,12 @@
-import { vitePlugin as remix } from '@remix-run/dev';
-import { installGlobals } from '@remix-run/node';
+import {
+	vitePlugin as remix,
+	cloudflareDevProxyVitePlugin,
+} from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import { cjsInterop } from 'vite-plugin-cjs-interop';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-installGlobals();
+import { getLoadContext } from './load-context';
 
 export default defineConfig(({ isSsrBuild }) => ({
 	server: {
@@ -12,8 +14,11 @@ export default defineConfig(({ isSsrBuild }) => ({
 	},
 	// We only need to target ES2022 for the server build
 	// for top-level await support
-	build: isSsrBuild ? { target: 'ES2022' } : {},
+	build: isSsrBuild ? { target: 'ES2022', minify: true } : { minify: true },
 	plugins: [
+		cloudflareDevProxyVitePlugin({
+			getLoadContext,
+		}),
 		remix({
 			postcss: true,
 		}),
@@ -22,4 +27,12 @@ export default defineConfig(({ isSsrBuild }) => ({
 		}),
 		tsconfigPaths(),
 	],
+	ssr: {
+		resolve: {
+			conditions: ['workerd', 'worker', 'browser'],
+		},
+	},
+	resolve: {
+		mainFields: ['browser', 'module', 'main'],
+	},
 }));
