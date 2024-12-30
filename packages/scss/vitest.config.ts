f5647e1d003380040fbb91ac2dc2f9d090bd4843
ayuhito <hello@ayuhito.com>
2024-12-31 01:42:09 +0900

    refactor(sass): use or operators in default variable setting

--- packages/scss/vitest.config.ts
+++ packages/scss/vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		clearMocks: true,
+		forceRerunTriggers: [
+			'**/*.scss',
+			'**/package.json/**',
+			'**/vitest.config.*/**',
+			'**/vite.config.*/**',
+		],
 	},
 });
