9ae62cc3810e670cde8de9e2c4d8aacdb9707669
Ayu <hello@ayuhito.com>
2024-12-30 13:23:21 +0900

    fix(website): invalid robots txt (#1009)

--- website/app/routes/[robots.txt].tsx
+++ website/app/routes/[robots.txt].tsx
@@ -1,26 +1,15 @@
 import type { LoaderFunction } from '@remix-run/cloudflare';
 
-export const loader: LoaderFunction = async () => {
-	const prod = `User-agent: *
+const prod = `User-agent: *
 Allow: /
 
 Sitemap: https://fontsource.org/sitemap.xml`;
 
-	const headers = {
-		'Content-Type': 'text/plain',
-		'Cache-Control': 'public, max-age=86400', // 1 day
-	};
-
-	if (process.env.FLY_APP_NAME === 'fontsource') {
-		return new Response(prod, {
-			headers,
-		});
-	}
-
-	const dev = `User-agent: *
-Disallow: /`;
-
-	return new Response(dev, {
-		headers,
+export const loader: LoaderFunction = async () => {
+	return new Response(prod, {
+		headers: {
+			'Content-Type': 'text/plain',
+			'Cache-Control': 'public, max-age=86400', // 1 day
+		},
 	});
 };
