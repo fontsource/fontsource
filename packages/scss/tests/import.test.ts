87badfd1d807c729233babc1fc6049c149d6ff7a
ayuhito <hello@ayuhito.com>
2025-01-12 14:43:40 +0900

    test: add import cases

---
+++ packages/scss/tests/import.test.ts
@@ -0,0 +1,30 @@
+import { compileString, NodePackageImporter } from 'sass';
+import { describe, expect, it } from 'vitest';
+
+const compileSass = (path: string) => {
+	const res = compileString(`@use ${path};`, {
+		importers: [new NodePackageImporter()],
+	});
+
+	return res.css.toString();
+};
+
+describe('scss imports', () => {
+	it('should import with default path', async () => {
+		expect(
+			compileSass("'pkg:@fontsource/carlito/index.css'"),
+		).toMatchSnapshot();
+	});
+
+	it('should import with default and namespace', async () => {
+		expect(
+			compileSass("'pkg:@fontsource/carlito/index.css' as carlito"),
+		).toMatchSnapshot();
+	});
+
+	it('should import with weight path and extension', async () => {
+		expect(
+			compileSass("'pkg:@fontsource/carlito/400.css' as carlito"),
+		).toMatchSnapshot();
+	});
+});
