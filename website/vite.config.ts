21cb6c5a7c5675621164d85b9103817bf230e26b
ayuhito <hello@ayuhito.com>
2025-08-28 08:29:35 +0900

    fix(website): use custom media

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,6 +1,9 @@
+import fs from 'node:fs';
 import { cloudflare } from '@cloudflare/vite-plugin';
 import mdx from '@mdx-js/rollup';
 import { reactRouter } from '@react-router/dev/vite';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 import remarkFrontmatter from 'remark-frontmatter';
@@ -11,12 +14,41 @@ import { defineConfig } from 'vite';
 import babel from 'vite-plugin-babel';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const targets = browserslistToTargets(
+	browserslist([
+		'chrome >= 107',
+		'edge >= 107',
+		'firefox >= 104',
+		'safari >= 16',
+	]),
+);
+
+const customMedia = fs.readFileSync('./app/styles/_media.css', 'utf-8');
+
 export default defineConfig({
-	css: { postcss: './postcss.config.cjs' },
 	build: {
+		cssMinify: 'lightningcss',
 		minify: 'terser',
 	},
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			targets,
+			drafts: {
+				customMedia: true,
+			},
+		},
+	},
 	plugins: [
+		{
+			name: 'css-additional-data',
+			enforce: 'pre',
+			transform(code, id) {
+				if (id.endsWith('.css') || id.endsWith('.css?inline')) {
+					return `${customMedia}\n${code}`;
+				}
+			},
+		},
 		cloudflare({ viteEnvironment: { name: 'ssr' } }),
 		mdx({
 			providerImportSource: '@mdx-js/react',
