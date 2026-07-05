0be81767c0b5692382dd8cca370336b209ecae92
ayuhito <hello@ayuhito.com>
2026-07-05 14:22:11 +0900

    chore(website): update deps

--- website/react-router.config.ts
+++ website/react-router.config.ts
@@ -3,6 +3,7 @@ import type { Config } from '@react-router/dev/config';
 export default {
 	ssr: true,
 	future: {
+		v8_splitRouteModules: true,
 		v8_viteEnvironmentApi: true,
 	},
 } satisfies Config;
