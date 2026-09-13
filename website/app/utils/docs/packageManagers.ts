e341c106b368d0bc651b18a71e83fcedfabcbbe0
ayuhito <hello@ayuhito.com>
2026-09-13 15:38:12 +0900

    fix(website): remove unused package manager helper

--- website/app/utils/docs/packageManagers.ts
+++ website/app/utils/docs/packageManagers.ts
@@ -9,12 +9,6 @@ export const packageManagerValues = packageManagers.map(
 	(manager) => manager.value,
 );
 
-export const getPackageManagerCommand = (value: string, cmd: string) =>
-	(
-		packageManagers.find((manager) => manager.value === value) ??
-		packageManagers[0]
-	).command(cmd);
-
 export const getPackageManagerCommands = (cmd: string) =>
 	packageManagers.map(({ command, value }) => ({
 		value,
